55479096ddb34c9d29c4768c57b3b23d1cfc3657
Fabio Albertin <albertin@gmail.com>
2019-08-16 00:30:27 +0100

    Finished setup and added basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Helvetic.dev</h1>
+        <nav>
+          <ul>
+            <li>Nav item 1</li>
+            <li>Nav item 2</li>
+          </ul>
+        </nav>
       </header>
+      <main className="clearFix">
+        <section>
+          Section 1
+        </section>
+        <section>
+          Section 2
+        </section>
+      </main>
+      <footer>
+        <p>
+            &copy; 2019 F.M. Albertin
+        </p>
+      </footer>
     </div>
   );
 }
