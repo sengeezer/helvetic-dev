27aa21cd747edc8f6e3c0d0879acedadd92ab7f1
Fabio Albertin <albertin@gmail.com>
2019-08-19 23:48:54 +0100

    Completed past work full time era data

--- src/content/pastWork.js
+++ src/content/pastWork.js
@@ -6,6 +6,7 @@ const pastWork = {
     projects: [
       {
         title: '',
+        link: 'https://www.circusstreet.com',
         time: '',
         text: '',
       },
@@ -16,6 +17,7 @@ const pastWork = {
     projects: [
       {
         title: 'Matchesfashion.com',
+        link: 'https://www.matchesfashion.com',
         time: '(October 2016 - June 2019)',
         text: 'Implement front end components of new features of online platform\n\nSupport maintenance of existing e-commerce features\n\nLead front end technology stack upgrade project\nTechnologies used: Hybris 5.x - 6.6, Webpack, ES2015+, Handlebars JS, SASS, Impex, Apache Velocity, jQuery',
       },
@@ -25,9 +27,44 @@ const pastWork = {
     summary: 'A pan-european relatforming / brownfielding project to transition Office Depot Europe to SAP\'s Hybris platform.',
     projects: [
       {
-        title: '',
+        title: 'Viking Direkt (Austrian imprint)',
+        link: 'https://www.vikingdirekt.at/en/',
+        time: '(June 2015 - October 2016)',
+        text: 'Implemented front end components of transitioned features of online platform\n\nSupported development of new e-commerce features\n\nTechnologies used: Hybris 5.x - 6, CanJS, Handlebars JS, SASS, Grunt, Phantom JS, Jasmine, jQuery',
+      },
+    ],
+  },
+  cog: {
+    summary: 'Design / develop Web pages using HTML5 / CSS3 / JS\n\nPrepare image assets from source files\n\nHelp back-end developers with integration\n\nUnit test deliverables',
+    projects: [
+      {
+        title: 'Apple Online Store',
+        link: 'http://store.apple.com/',
+        time: '(November 2014 - April 2015)',
+        text: 'Implement front end components of new features of online store\n\nSupport reduction of backlog of improvements to online store\n\nTechnologies used: CanJS, Mustache JS, Grunt, Phantom JS, Jasmine, jQuery',
+      },
+      {
+        title: 'O2',
+        link: 'http://www.o2.co.uk/',
+        time: '(August 2014 - October 2014)',
+        text: 'Migrated existing content into custom CMS\n\nImplemented marketing initiative assets in CMS\n\nPrepared new templates for CMS and e-mail marketing\n\nTechnologies used: Apache Velocity, SASS, Grunt, jQuery',
+      },
+    ],
+  },
+  zappos: {
+    summary: '',
+    projects: [
+      {
+        title: '6pm.com',
+        link: 'http://www.6pm.com/',
         time: '',
-        text: '',
+        text: 'Company\'s discount retail site.\n\nLed and coded site revamp in Fall 2008.',
+      },
+      {
+        title: 'Zappos Blogs',
+        link: 'http://blogs.zappos.com/',
+        time: '(August 2014 - October 2014)',
+        text: 'Company\'s blogs on business related topics and company culture.\n\nEstablished blog lineup and significantly improved existing code.',
       },
     ],
   },
