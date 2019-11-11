b3d8e98e1c46556309ba0678800ac7d50da46dd5
Fabio Albertin <albertin@gmail.com>
2019-11-11 02:41:27 +0000

    Completed initial version of projects section

--- src/components/PastWork.js
+++ src/components/PastWork.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
 import work from '../content/pastWork';
+import projectsData from '../content/projects';
 
 import Accordion from './Accordion';
 
-const PastWork = () => (
-  <div className="pastWork">
-    <h2>{work.title}</h2>
-    <p>{work.intro}</p>
-    <Accordion data={work.positions} />
-  </div>
-);
+const PastWork = () => {
+  const { projects } = projectsData;
+  return (
+    <div className="pastWork">
+      <div className="employment">
+        <h2>{work.title}</h2>
+        <p>{work.intro}</p>
+        <Accordion data={work.positions} />
+      </div>
+      <div className="projects">
+        <h2>{projectsData.title}</h2>
+        <p>{projectsData.intro}</p>
+        <ul className="projects__list">
+        {projects.map((project, pKey) => {
+          const key1 = pKey + Math.random() * 10;
+          return (
+            <li key={key1}>
+              <h4>{project.title}</h4>
+              <p>{project.description}</p>
+              <a href={project.url}>{project.url}</a>
+            </li>
+          );
+        })}
+        </ul>
+      </div>
+    </div>
+  )
+};
 
 export default PastWork;
---
+++ src/content/projects.js
@@ -0,0 +1,18 @@
+const projects = {
+  title: 'Projects',
+  intro: 'The following are some projects I have created to demonstrate skills I have acquired outside of formal work.',
+  projects: [
+    {
+      title: 'Simple Github Search',
+      description: 'A simple Github user search app with different display modes.',
+      url: 'https://sengeezer.github.io/gh-search/'
+    },
+    {
+      title: 'React Audio Player',
+      description: 'An basic audio player based on HTML5 audio.',
+      url: 'https://sengeezer.github.io/react-audio-player/'
+    }
+  ]
+};
+
+export default projects;
