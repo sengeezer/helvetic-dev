016a82aa41e75772aafa01485ea5860ea7322368
Fabio Albertin <fabio.albertin@sainsburys.co.uk>
2025-06-03 19:54:20 +0100

    Migration fixes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Header from '../components/Header';
-import Main from '../components/Main';
-import Footer from '../components/Footer';
+import Header from '../src/components/Header';
+import Main from '../src/components/Main';
+import Footer from '../src/components/Footer';
 
 export default function Home() {
   return (
